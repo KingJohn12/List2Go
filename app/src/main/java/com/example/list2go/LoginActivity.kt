31c5d5c133e1e62a1e6ebe9468fa9f8302b8c2a7
smahavadi <svmahavadi@gmail.com>
2022-04-15 16:11:26 -0400

    added logo image and customized login page

--- app/src/main/java/com/example/list2go/LoginActivity.kt
+++ app/src/main/java/com/example/list2go/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.example.list2go
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+            loginUser(username, password)
+        }
+
+    }
+
+    private fun loginUser(username: String, password: String) {
+
+
+
     }
 }
